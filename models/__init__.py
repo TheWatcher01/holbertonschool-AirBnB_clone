f001a824243d5ce75c8e44daef8e40e7d7078b55
TheWatcher01 <teddydeberdt@gmail.com>
2024-03-05 16:32:21 +0100

    Add FileStorage instance and reload data from file

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 """This module contains the __init__ file for the AirBnB clone"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()  # Create instance of FileStorage
+storage.reload()  # Reload data from file
